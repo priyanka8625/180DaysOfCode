a730509f7ba57216610bfabfe38f43df57b0e41f
AnkitaAdam <ksundalam@gmail.com>
2023-10-14 18:47:05 +0530

    Day27 commited!

---
+++ Day027 - Solving Leetcode Ques/FindCommonNodesInTwoBSTs.cpp
@@ -0,0 +1,33 @@
+//Given two Binary Search Trees. Find the nodes that are common in both of them, ie- find the intersection of the two BSTs.
+//
+//Note: Return the common nodes in sorted order.
+class Solution
+{
+    public:
+    //Function to find the nodes that are common in both BST. 
+    void helper(Node *root,unordered_map<int,int>&mp)
+    {
+        if(root) {
+            helper(root->left,mp);
+            mp[root->data]++;
+            helper(root->right,mp);
+        }
+    }
+    void helper2(Node *root2, unordered_map<int, int>&mp,vector<int>&ans)
+    {
+        if(root2) {
+            helper2(root2->left,mp,ans);
+            if(mp.find(root2->data) != mp.end()) ans.push_back(root2->data);
+            helper2(root2->right,mp,ans);
+            
+        }
+    }
+    vector <int> findCommon(Node *root1, Node *root2)
+    {
+     unordered_map<int,int> mp;
+     vector<int> ans;
+     helper(root1,mp);
+     helper2(root2,mp,ans);
+     return ans;
+    }
+};
