94045ed3667eefd95c57b2c0cc593833075eb4ac
AnkitaAdam <ksundalam@gmail.com>
2023-11-11 14:01:39 +0530

    Day54 updated!

---
+++ Day054 - Factorial of Large Number, Integer to Roman, Roman to Integer/GFG_Factorials of large numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> factorial(int N){
+        vector<int> ans(1,1);
+        while(N>1){
+            int carry =0, res=0, size=ans.size();
+            for(int i=0; i<size; i++){
+                res = ans[i] * N + carry;
+                carry = res/10;
+                ans[i] = res%10;
+            }
+            while(carry){
+                ans.push_back(carry%10);
+                carry/=10;
+            }
+            N--;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
