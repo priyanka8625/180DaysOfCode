4735b49f8c43d0be69d02e253cd8d8002e3b03f7
AnkitaAdam <ksundalam@gmail.com>
2023-12-29 20:03:08 +0530

    Day96 updated!

---
+++ Day096 - Recursion series end here/GFG_Print N-bit binary numbers having more 1s than 0s_baseCondn2.cpp
@@ -0,0 +1,31 @@
+//User function template for C++
+class Solution{
+public:	
+    
+	vector<string> NBitBinary(int N)
+	{
+	    vector<string> ans;
+	    //generate binary bits 
+	    find(ans, "", 0, 0, N);
+	    return ans;
+	}
+	void find(vector<string> &ans, string temp, int ones, int zeros, int n){
+	    if(n == 0){
+	        ans.push_back(temp);
+	        return;
+	    }
+	    
+	    //include 1 bit in temp, temp indicates binary number here
+	    temp.push_back('1');
+	    find(ans, temp, ones+1, zeros, n-1);//icrease the count of 1  and generate numbers with two choices again 0 and 1
+	    temp.pop_back();
+	    
+	    //include zeroes in the combination of 0 and 1 only if ones>zeros
+	    if(ones>zeros){
+	        //if temp satisfies the condition of prefixes having ones > zeros
+	        temp.push_back('0');
+	        find(ans, temp, ones, zeros+1, n-1);//increase the count of 0s and generate numbers with two choices again 0 and 1
+	        temp.pop_back();
+	    }
+	}
+};
