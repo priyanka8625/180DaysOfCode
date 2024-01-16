c01c5d627657861206a10f62f143f7a33ef8766a
AnkitaAdam <ksundalam@gmail.com>
2024-01-16 08:48:17 +0530

    insert_mid in LL added

---
+++ Day112 - Introduction to Linked List, Traversal and Insertion/insertAtMiddle_inExistingLL.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+	public:
+		int data;
+		Node* next;
+		Node(int data){
+			this->data = data;
+			this->next = NULL;
+		}
+};
+
+void insert_mid(Node* &head, int pos, int data){
+	if(head == NULL){
+		cout<<"List is empty!"<<endl;
+		return;
+	}
+	Node *p = head;
+	while(pos--){
+		//reach out to the pos'th node
+		p = p->next;
+	}
+	// now insert the node
+	Node *temp = new Node(data);
+	temp->next = p->next;
+	p->next = temp;
+	cout<<"Inserted!"<<endl;
+}
+
+Node* createLL(int arr[], int index, int n, Node* prev){
+	if(index == n){
+		return prev;
+	}
+	Node *temp = new Node(arr[index]);
+	temp->next = prev;
+	return createLL(arr, index+1, n, temp);
+}
+void print(Node* head){
+//	any changes in head pointer of the print function won't reflect the changes in head of main function 
+//	in such cases, we can use the & reference operator, reflect the changes
+	cout<<"List : "<<endl;
+	while(head!=NULL){
+		cout<<head->data<<" ";
+		head = head->next;
+	}
+	cout<<endl;
+}
+int main(){
+	Node *head=NULL;
+	int arr[5] = {1,2,3,4,5};
+	
+	//create a linked list using array first
+	head = createLL(arr, 0, 5, NULL);
+	print(head);
+	//then, insert element
+	int x,data;
+	cout<<"Enter position of the node after which you want to insert(starting from 1): "<<endl;
+	cin>>x;
+	cout<<"Enter data to be inserted: "<<endl;
+	cin>>data;
+	insert_mid(head, x-1, data);
+	print(head);
+	return 0;
+}
