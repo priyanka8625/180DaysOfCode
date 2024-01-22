13656f52f970ae656fab0aec0517518cfaf7fced
AnkitaAdam <ksundalam@gmail.com>
2024-01-22 23:15:11 +0530

    delete a node without head pointer soln added

---
+++ Day114 - Deleting a node from Linked List/Delete without head pointer.cpp
@@ -0,0 +1,26 @@
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}*head;
+*/
+
+
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del)
+    {
+       // Your code here
+       Node *nextNode = del->next;
+       del->data = nextNode->data;
+       del->next = nextNode->next;
+       delete nextNode;
+    }
+
+};
